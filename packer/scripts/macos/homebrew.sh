aa7f9622945b8c0e0473eb04b5a203f778361e74
call <6537446+call@users.noreply.github.com>
2020-01-07 19:38:09 -0800

    Fix CLT install script

--- packer/scripts/macos/homebrew.sh
+++ packer/scripts/macos/homebrew.sh
@@ -6,8 +6,18 @@ echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile
 echo "export PATH=/usr/local/bin:$PATH" >> ~/.zshenv
 source ~/.bash_profile
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Manually install xcode dev tools, it fails sometimes in the homebrew installer
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 
-# Install https://github.com/Homebrew/homebrew-bundle
-brew bundle
+# This one-liner brought to you by the Homebrew install script
+PROD=$(/usr/sbin/softwareupdate -l |
+  grep -B 1 -E 'Command Line Tools' |
+  awk -F'*' '/^ *\\*/ {print $2}' |
+  sed -e 's/^ *Label: //' -e 's/^ *//' |
+  sort -V |
+  tail -n1)
+softwareupdate -i "$PROD";
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle
