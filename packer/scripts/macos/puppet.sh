05800658a30d6525521700d9d967df37d7d1bcbe
call <6537446+call@users.noreply.github.com>
2020-01-10 12:44:55 -0800

    Set lang in ~/.bash_profile

--- packer/scripts/macos/puppet.sh
+++ packer/scripts/macos/puppet.sh
@@ -3,3 +3,6 @@ brew cask install puppet-agent-6
 
 # Puppet module installs
 sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
+
+# Set locale
+echo 'export LANG="en_US.UTF-8"' >> ~/.bash_profile
