029a6da6c57f50ba5a91b088a2d4cd41241cafa5
call <6537446+call@users.noreply.github.com>
2020-01-10 14:34:48 -0800

    Attempt to fix locale in puppet

--- packer/puppet/macos/tools.pp
+++ packer/puppet/macos/tools.pp
@@ -1,3 +1,9 @@
+
+# Set locale to address https://tickets.puppetlabs.com/browse/PUP-5042
+exec { 'set_locale' :
+  command => "/bin/bash -c \"export LANG=en_us.UTF-8\"",
+}
+
 class { 'homebrew':
   user => 'anka',
 }
@@ -7,7 +13,8 @@ package { 'serverspec':
   provider => 'gem',
 }
 
-# package { 'autopkgr':
-#   ensure   => 'present',
-#   provider => 'brewcask',
-# }
+package { 'autopkgr':
+  ensure   => 'present',
+  provider => 'brewcask',
+  require  => Exec['set_locale'],
+}
--- packer/scripts/macos/puppet.sh
+++ packer/scripts/macos/puppet.sh
@@ -6,7 +6,3 @@ brew cask install puppet-agent-6
 
 # Puppet module installs
 sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
-
-# Set locale to address https://tickets.puppetlabs.com/browse/PUP-5042
-echo 'export LC_ALL=en_US.UTF-8' >> ~/.bash_profile
-echo 'export LC_ALL=en_US.UTF8' >> ~/.zshrc
