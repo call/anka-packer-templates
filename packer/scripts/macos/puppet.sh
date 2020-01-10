35be0dae569e19c8b4838103c630a97d8235c607
call <6537446+call@users.noreply.github.com>
2020-01-10 13:35:50 -0800

    Attempt to fix locale

--- packer/scripts/macos/puppet.sh
+++ packer/scripts/macos/puppet.sh
@@ -8,8 +8,20 @@ brew cask install puppet-agent-6
 sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
 
 # Set locale to address https://tickets.puppetlabs.com/browse/PUP-5042
-echo 'export LANG="en_US.UTF-8"' >> ~/.bash_profile
-echo 'export LC_ALL="en_US.UTF-8"' >> ~/.bash_profile
+echo 'export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+' >> ~/.bash_profile
 
-echo 'export LANG="en_US.UTF-8"' >> ~/.zshrc
-echo 'export LC_ALL="en_US.UTF-8"' >> ~/.zshrc
+echo 'export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+' >> ~/.zshrc
