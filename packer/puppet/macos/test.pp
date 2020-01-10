e4f7f74ad1a220660b6df6d9cf5c3071c13a1822
call <6537446+call@users.noreply.github.com>
2020-01-10 12:00:08 -0800

    Add explicit puppet apply

--- packer/puppet/macos/test.pp
+++ packer/puppet/macos/test.pp
@@ -1,6 +1,12 @@
 $pkgs = ['autopkgr']
+$gems = ['serverspec']
 
 package { $pkgs:
   ensure   => 'present',
   provider => 'homebrew',
+}
+
+package { $gems:
+  ensure   => 'present',
+  provider => 'gem',
 }
