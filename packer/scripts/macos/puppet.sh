e42db3e4d37c354c99cb68bd7b1e57f08dd3f0b9
call <6537446+call@users.noreply.github.com>
2020-01-10 12:54:26 -0800

    Add link to puppet JIRA

--- packer/scripts/macos/puppet.sh
+++ packer/scripts/macos/puppet.sh
@@ -4,5 +4,5 @@ brew cask install puppet-agent-6
 # Puppet module installs
 sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
 
-# Set locale
+# Set locale to address https://tickets.puppetlabs.com/browse/PUP-5042
 echo 'export LANG="en_US.UTF-8"' >> ~/.bash_profile
