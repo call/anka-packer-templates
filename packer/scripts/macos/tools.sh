2db84cc0d6debf2a0dd6a5114497d3a1dc05641f
call <6537446+call@users.noreply.github.com>
2020-01-10 12:17:38 -0800

    Explicitly set puppet bin dir

--- packer/scripts/macos/tools.sh
+++ packer/scripts/macos/tools.sh
@@ -1,8 +1,5 @@
 brew tap puppetlabs/puppet
 brew cask install puppet-agent-6
 
-echo 'alias puppet="/opt/puppetlabs/puppet/bin/puppet"' >> ~/.bash_profile
-echo 'alias puppet="/opt/puppetlabs/puppet/bin/puppet"' >> ~/.zshrc
-
 # Puppet module installs
 /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
