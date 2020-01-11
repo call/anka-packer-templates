01e7e6ebe8642e201cfe6973faf3a09aa43a53c5
call <6537446+call@users.noreply.github.com>
2020-01-10 17:07:38 -0800

    Move module versions to top of script

--- packer/scripts/macos/puppet.sh
+++ packer/scripts/macos/puppet.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+HOMEBREW_MODULE_VERSION='1.8.3'
+VSCREPO_MODULE_VERSION='3.1.0'
+MACDEFAULTS_MODULE_GIT_REF='35ca6165d39808aeffe40c89eceb272b5de4da5b'
+
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew tap puppetlabs/puppet
 brew cask install puppet-agent-6
 
 # Puppet module installs using CLI
-sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version 1.8.3
-sudo /opt/puppetlabs/puppet/bin/puppet module install puppetlabs-vcsrepo --version 3.1.0
+sudo /opt/puppetlabs/puppet/bin/puppet module install thekevjames-homebrew --version "${HOMEBREW_MODULE_VERSION}"
+sudo /opt/puppetlabs/puppet/bin/puppet module install puppetlabs-vcsrepo --version "${VCSREPO_MODULE_VERSION}"
+
+# Get script dir, make tmp dir, for module cloning
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORK_DIR="$(mktemp -d -p "${DIR}")"
 
 # Puppet module installs using Git, pinned to a ref
-MACDEFAULTS_GIT_REF='35ca6165d39808aeffe40c89eceb272b5de4da5b'
-sudo git clone https://github.com/puppetlabs/puppet-macdefaults.git /etc/puppetlabs/code/modules/macdefaults
-sudo cd /etc/puppetlabs/code/modules/macdefaults
-sudo git reset --hard $MACDEFAULTS_GIT_REF
+sudo git clone https://github.com/puppetlabs/puppet-macdefaults.git $WORK_DIR
+sudo git reset --hard $MACDEFAULTS_MODULE_GIT_REF
+sudo mv $WORK_DIR /etc/puppetlabs/code/modules/macdefaults
