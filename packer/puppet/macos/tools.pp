c3aa4c86d6e01d43cc012e1fd6bee1900832b722
call <6537446+call@users.noreply.github.com>
2020-01-10 16:19:30 -0800

    Add tccprofile

--- packer/puppet/macos/tools.pp
+++ packer/puppet/macos/tools.pp
@@ -1,6 +1,7 @@
 $autopkg_version = '1.4.1'
 $jssimporter_version = '1.0.5'
 $serverspec_version = '2.36.0'
+$tccprofile_ref = '487e121275ff98e532e5b44fcbf85d0aca375ed8'
 
 package { 'serverspec':
   ensure   => $serverspec_version,
@@ -12,13 +13,19 @@ package { 'autopkg':
   source => "https://github.com/autopkg/autopkg/releases/download/v${autopkg_version}/autopkg-${autopkg_version}.pkg",
 }
 
+package { 'autopkgr':
+  ensure   => 'present',
+  provider => 'brewcask',
+}
+
 package { 'jssimporter':
   ensure => 'installed',
   source => "https://github.com/jssimporter/JSSImporter/releases/download/v${jssimporter_version}/jssimporter-${jssimporter_version}.pkg"
 }
 
-package { 'autopkgr':
+vcsrepo { '/opt/tccprofile':
   ensure   => 'present',
-  provider => 'brewcask',
-  require  => Package['autopkg'],
+  provider => 'git',
+  source   => 'https://github.com/carlashley/tccprofile.git',
+  revision => $tccprofile_ref,
 }
